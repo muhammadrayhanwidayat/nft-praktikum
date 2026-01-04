f747d65b3668c937a1ee497346c01ec38c7b7438
muhammadrayhanwidayat <murayat99@gmail.com>
2026-01-04 09:37:26 +0700

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          [process.env.PRIVATE_KEY],       // Private key array
+          process.env.RPC_URL              // INFURA atau RPC publik
+        ),
+      network_id: 11155111,
+      gas: 5000000,
+      gasPrice: 30000000000,               // 30 gwei
+      confirmations: 2,
+      timeoutBlocks: 200,
+      networkCheckTimeout: 100000,         // ← penting supaya TIDAK TIMEOUT
+      skipDryRun: true
+    }
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.20",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+  }
+};
